b222efc19982d8860ab96730be952dea44ecb5d5
jammingbadger <christofer2010@live.se>
2025-06-08 18:55:27 +0200

    Fixed background image and hero content

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,15 +14,16 @@ const Home = () => {
   };
 
   return (
-    <section id="hem" className="hero">
-      <div className="hero-content">
+    <section id="hem" className="home">
+      <div className="overlay">
+      <div className="home-content">
       {/* <img src={logo2} alt="Logotyp2" className="logo2" /> */}
         <h1>Din Mäklare i Åhus & Österlen</h1>
         <p>Ditt hem. Din framtid. Vår passion.</p>
-           
-            <button
+
+        <button
               
-              className={`btn ${activeSection === "fastigheter" ? "active" : ""}`}
+              className={`hero-button ${activeSection === "fastigheter" ? "active" : ""}`}
               onClick={(e) => {
                 e.preventDefault();
                 setActiveSection("fastigheter");
@@ -31,7 +32,8 @@ const Home = () => {
             >
               Bostäder till salu
             </button>
-        
+
+      </div>
       </div>
     </section>
   );
